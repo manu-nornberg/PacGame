9c4d29a61559aaa4054a30102f2f4540330d7b69
emanuelleporto10@gmail.com <emanuelleporto10@gmail.com>
2023-05-28 11:57:43 -0300

    ajustes finais

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './styles/style.css'
 import {init} from "./game.js"
 
-window.addEventListener("load", init, false)
+// window.addEventListener("load", init, false)
+
+let botao = document.querySelector("#jogar")
+botao.addEventListener('click', init,false)
