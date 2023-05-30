448a588002d70dc0fb18fabae5b1d84d6cff4dc2
emanuelleporto10@gmail.com <emanuelleporto10@gmail.com>
2023-05-30 13:59:31 -0300

    resolvendo circle

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import {init} from "./game.js"
 // window.addEventListener("load", init, false)
 
 let botao = document.querySelector("#jogar")
-botao.addEventListener('click', init,false)
+botao.addEventListener('click', init, false)
